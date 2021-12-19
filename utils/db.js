caa7480dc782eb6f257f77bf9face069d010965b
niharika rastogi <rastoginiha2002@gmail.com>
2021-12-19 17:42:47 +0530

    deploying

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize');
+const isProduction = process.env.NODE_ENV === 'production';
+const connectionString = `postgresql://${process.env.PGUSER}:${process.env.PGPASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.PGDB}`;
+
 const sequelize = new Sequelize(process.env.PGDB,process.env.PGUSER,process.env.PGPASS, {
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction,
     dialect: 'postgres',
     logging: false
 });
