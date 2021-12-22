c3e8e55bf42811fa45b432eca981db73c8fa529c
niharika rastogi <rastoginiha2002@gmail.com>
2021-12-22 17:25:57 +0530

    changes in db

--- utils/db.js
+++ utils/db.js
@@ -1,8 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PGDB,process.env.PGUSER,process.env.PGPASS, {
-    connectionString: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    logging: false
-});
+// const sequelize = new Sequelize(process.env.PGDB,process.env.PGUSER,process.env.PGPASS, {
+//     connectionString: process.env.DATABASE_URL,
+//     dialect: 'postgres',
+//     logging: false
+// });
+
+const sequelize = new Sequelize("postgres://ksbclekwesajle:009f187861b605812dce641e30c2e436a3141c034e240d32a937a7798c1fb5e6@ec2-52-54-38-229.compute-1.amazonaws.com:5432/dflrmfcvojgh3v");
 module.exports = sequelize;
