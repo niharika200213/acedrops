fab603e4d3258ef4611c7503e908bebfbbe6ae9b
niharika rastogi <rastoginiha2002@gmail.com>
2021-12-22 17:27:01 +0530

    changes in db

--- utils/db.js
+++ utils/db.js
@@ -6,5 +6,5 @@ const Sequelize = require('sequelize');
 //     logging: false
 // });
 
-const sequelize = new Sequelize("postgres://ksbclekwesajle:009f187861b605812dce641e30c2e436a3141c034e240d32a937a7798c1fb5e6@ec2-52-54-38-229.compute-1.amazonaws.com:5432/dflrmfcvojgh3v");
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 module.exports = sequelize;
