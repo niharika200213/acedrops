9e7d97a2c6e2d46c0480ab7e7205b9dc9fb2ab0f
niharika rastogi <rastoginiha2002@gmail.com>
2022-01-05 20:53:09 +0530

    update address and phno

--- routes/user.js
+++ routes/user.js
@@ -9,4 +9,12 @@ router.get('/search', userControllers.search);
 
 router.post('/reviews/:prodId', isAuth, userControllers.rate);
 
+router.post('/addAddress', isAuth, userControllers.addAddress);
+
+router.post('/addPhno', isAuth, userControllers.addPhno);
+
+router.get('/getAddress', isAuth, userControllers.getAddress);
+
+router.post('/orderCart', isAuth, userControllers.orderCart);
+
 module.exports = router;
